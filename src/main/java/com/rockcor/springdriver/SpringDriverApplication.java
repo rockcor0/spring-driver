9036330605a001cffabb04b09e6fd057e83c06a3
Ricardo Delgado <ridel007@gmail.com>
2021-01-14 21:05:43 -0500

    First

---
+++ src/main/java/com/rockcor/springdriver/SpringDriverApplication.java
@@ -0,0 +1,13 @@
+package com.rockcor.springdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockcor/springdriver/SpringDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockcor.springdriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
